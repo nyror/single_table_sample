936a3c6adf0db15696b5c2b792d17515161b0e12
Ken Lee <klee589@bloomberg.net>
2013-10-25 15:52:32 -0400

    added hours calculation

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,2 +1,34 @@
-class Timesheet < ActiveRecord::Base
+class Timesheet < ActiveRecord::Base 
+  def billable_hours_outstanding
+    if submitted?
+      billable_weeks.map(&:total_hours).sum
+    else
+      0
+    end
+  end
+
+  # non-relevant code ommitted
+  def self.billable_hours_outstanding_for(user)   
+    user.timesheets.map(&:billable_hours_outstanding).sum
+  end 
+end
+
+class DraftTimesheet < Timesheet
+  def billable_hours_outstanding
+    0
+  end
+end
+
+class SubmittedTimesheet < Timesheet
+  def billable_hours_outstanding
+    billable_weeks.map(&:total_hours).sum
+  end
+end
+
+class PaidTimesheet < Timesheet 
+  
+  def billable_hours_outstanding
+    paid_hours = 5
+    billable_weeks.map(&:total_hours).sum - paid_hours 
+  end
 end
